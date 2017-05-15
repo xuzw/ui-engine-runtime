6b3e8bb6a585a8e0eecae2e66bcb3ce3fcd124ef
xuzewei <xuzewei_2012@126.com>
2017-05-15 14:49:40 +0800

    ui-engine.js编码中文cookie

--- src/main/java/com/github/xuzw/ui_engine_runtime/script/ui-engine.js
+++ src/main/java/com/github/xuzw/ui_engine_runtime/script/ui-engine.js
@@ -1,44 +1,43 @@
 var UiEngine = {
-	click : function(source) {
-		var div = source.parentNode;
-		cookie.set('uiEngine.event.type', source.dataset.clickEventClass);
-		cookie.set('uiEngine.event.location.className', div.className);
-		cookie.set('uiEngine.event.location.id', div.id);
-		this.updateCookies();
-		this.refresh();
-	},
-	updateCookies : function() {
-		var inputs = document.getElementsByTagName("input");
-		for (var i = 0; i < inputs.length; i++) {
-			var input = inputs[i];
-			cookie.set('uiEngine.input.' + input.id, input.value);
-		}
-	},
-	deleteCookies : function() {
-		cookie.remove('uiEngine.event.type');
-		var cookies = cookie.all();
-		for ( var x in cookies) {
-			if (x.startsWith('uiEngine.input.')) {
-				cookie.remove(x);
-			}
-		}
-	},
-	refresh : function() {
-		window.location.reload();
-	},
-	init : function() {
-		this.deleteCookies();
-	},
-	closePopup : function() {
-		for (let x of document
-				.getElementsByClassName("com-github-xuzw-ui_engine_runtime-div-MaskDiv")) {
-			x.parentNode.removeChild(x);
-		}
-		for (let x of document
-				.getElementsByClassName("com-github-xuzw-ui_engine_runtime-div-wrapper-PopupDivWrapper")) {
-			x.parentNode.removeChild(x);
-		}
-	}
+    click: function(source) {
+        var div = source.parentNode;
+        cookie.set('uiEngine.event.type', source.dataset.clickEventClass);
+        cookie.set('uiEngine.event.location.className', div.className);
+        cookie.set('uiEngine.event.location.id', div.id);
+        this.updateCookies();
+        this.refresh();
+    },
+    updateCookies: function() {
+        var inputs = document.getElementsByTagName('input');
+        for (var i = 0; i < inputs.length; i++) {
+            var input = inputs[i];
+            if (input.id != '') {
+                cookie.set('uiEngine.input.' + input.id, encodeURIComponent(input.value));
+            }
+        }
+    },
+    deleteCookies: function() {
+        var cookies = cookie.all();
+        for (var x in cookies) {
+            if (x.startsWith('uiEngine.')) {
+                cookie.remove(x);
+            }
+        }
+    },
+    refresh: function() {
+        window.location.reload();
+    },
+    init: function() {
+        this.deleteCookies();
+    },
+    closePopup: function() {
+        for (let x of document.getElementsByClassName('com-github-xuzw-ui_engine_runtime-div-MaskDiv')) {
+            x.parentNode.removeChild(x);
+        }
+        for (let x of document.getElementsByClassName('com-github-xuzw-ui_engine_runtime-div-wrapper-PopupDivWrapper')) {
+            x.parentNode.removeChild(x);
+        }
+    }
 }
 
 UiEngine.init();
